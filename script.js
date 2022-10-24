913344a9ef2e1b9ee2c13946b7df7d1c9276d75b
yuza <akhmadyuza@gmail.com>
2022-10-24 09:29:46 +0700

    listening on click when user click on header img social

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 'use strict'
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('everything laoded')
+    let imgSocialLink = document.getElementsByClassName('link')
+    imgSocialLink[0].addEventListener('click', function(){
+        window.open('http://github.com/yvza', '_blank').focus()
+    })
+    imgSocialLink[1].addEventListener('click', function(){
+        window.open('https://stackoverflow.com/users/8111050/yuza', '_blank').focus()
+    })
 })
