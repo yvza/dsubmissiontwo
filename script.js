4737598fef11a4a8f2793bc2745c4c94b71e787a
yuza <akhmadyuza@gmail.com>
2022-10-24 14:06:05 +0700

    onclick action when adding new book

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import { addBook } from './modules/addBook.js'
+
 window.addEventListener('DOMContentLoaded', (event) => {
     let imgSocialLink = document.getElementsByClassName('link')
     imgSocialLink[0].addEventListener('click', function(){
@@ -7,5 +9,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
     })
     imgSocialLink[1].addEventListener('click', function(){
         window.open('https://stackoverflow.com/users/8111050/yuza', '_blank').focus()
+    })
+
+    let getAddBookButton = document.getElementById('addBook')
+    getAddBookButton.addEventListener('click', function(e){
+        e.preventDefault()
+        let getJudul = document.getElementById('judul').value
+        let getPenulis = document.getElementById('penulis').value
+        let getTahun = document.getElementById('tahun').value
+        let getStatus = document.getElementById('legible-checkbox').checked
+        addBook(getJudul, getPenulis, getTahun, getStatus)
+    })
+
+    let getSearchBookButton = document.getElementById('searchBook')
+    getSearchBookButton.addEventListener('click', function(e){
+        e.preventDefault()
+        let getSearchedString = document.getElementById('searchBook').value
+        console.log(getSearchedString)
     })
 })
