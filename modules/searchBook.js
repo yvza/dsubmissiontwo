17f7e247074ece51bce39cb26da96016fa657b22
yuza <akhmadyuza@gmail.com>
2022-10-25 14:04:05 +0700

    adding guard when search string empty

--- modules/searchBook.js
+++ modules/searchBook.js
@@ -1,25 +1,31 @@
 import { clearBook } from '../utils/clearBook.js'
 import { renderBooksToDom } from '../utils/renderBooksToDom.js'
+import { loadBook } from './loadBook.js'
 
 export async function searchBook(userInput) {
-    if (localStorage.getItem('books')) {
-        let books = JSON.parse(localStorage.getItem('books'))
-        let searchedStringIndex = await books.findIndex(res => {
-            return res.title === userInput
-        })
-        if (typeof books[searchedStringIndex] != 'undefined') {
-            console.log(books[searchedStringIndex])
-            clearBook()
-            renderBooksToDom([books[searchedStringIndex]], 1)
+    if (userInput != '') {
+        if (localStorage.getItem('books')) {
+            let books = JSON.parse(localStorage.getItem('books'))
+            let searchedStringIndex = await books.findIndex(res => {
+                return res.title === userInput
+            })
+            if (typeof books[searchedStringIndex] != 'undefined') {
+                console.log(books[searchedStringIndex])
+                clearBook()
+                renderBooksToDom([books[searchedStringIndex]], 1)
+            } else {
+                clearBook()
+                renderBooksToDom(books, 0)
+                // console.log('Buku tidak ditemukan')
+            }
         } else {
             clearBook()
-            renderBooksToDom(books, 0)
-            // console.log('Buku tidak ditemukan')
+            renderBooksToDom([], 0)
+            // console.log('Data buku masih kosong')
+            // data buku masih kosong
         }
     } else {
         clearBook()
-        renderBooksToDom([], 0)
-        // console.log('Data buku masih kosong')
-        // data buku masih kosong
+        loadBook()
     }
 }
