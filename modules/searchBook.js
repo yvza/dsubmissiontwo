f39aea63fe59307b972030dbfd4533173147ca08
yuza <akhmadyuza@gmail.com>
2022-10-25 09:13:08 +0700

    book search functionality

--- modules/searchBook.js
+++ modules/searchBook.js
@@ -7,12 +7,15 @@ export function searchBook(userInput) {
         if (typeof books[searchedStringIndex] != 'undefined') {
             console.log(books[searchedStringIndex])
             clearBook()
-            renderBooksToDom(books)
+            renderBooksToDom([books[searchedStringIndex]], 1)
         } else {
-            console.log('Buku tidak ditemukan')
+            clearBook()
+            renderBooksToDom(books, 0)
+            // console.log('Buku tidak ditemukan')
         }
     } else {
-        console.log('Data buku masih kosong')
+        renderBooksToDom([], 0)
+        // console.log('Data buku masih kosong')
         // data buku masih kosong
     }
 }
@@ -24,8 +27,68 @@ function clearBook(){
     while (legible.firstChild) legible.removeChild(legible.firstChild)
 }
 
-function renderBooksToDom(books){
-    books.map(res => {
+function renderBooksToDom(books, isFound){
+    let unreadBookDom = document.getElementById('unread-data')
+    let legibleBookDom = document.getElementById('legible-data')
+    
+    let unreadBookItemHtml = ""
+    let legibleBookItemHtml = ""
 
-    })
+    if (isFound) {
+        books.map(res => {
+            if (res.isComplete) {
+                legibleBookItemHtml += "<section class='book'> \
+                    <section> \
+                        <h1>"+res.title+"</h1> \
+                        <h6>Penulis: "+res.author+"</h6> \
+                        <h6>Tahun: "+res.year+"</h6> \
+                    </section> \
+                    <section class='control'> \
+                        <button type='button' value='"+res.id+"'>Tandai Belum Dibaca</button> \
+                        <button type='button' value='"+res.id+"'>Hapus Buku</button> \
+                    </section> \
+                </section>"
+            } else {
+                unreadBookItemHtml += "<section class='book'> \
+                    <section> \
+                        <h1>"+res.title+"</h1> \
+                        <h6>Penulis: "+res.author+"</h6> \
+                        <h6>Tahun: "+res.year+"</h6> \
+                    </section> \
+                    <section class='control'> \
+                        <button type='button' value='"+res.id+"'>Tandai Belum Dibaca</button> \
+                        <button type='button' value='"+res.id+"'>Hapus Buku</button> \
+                    </section> \
+                </section>"
+            }
+        })
+    }
+
+    if (books.length == 0) {
+        unreadBookItemHtml = "<section class='book'> \
+            <section> \
+                <h1>Data buku masih kosong</h1> \
+            </section> \
+        </section>"
+        legibleBookItemHtml = "<section class='book'> \
+            <section> \
+                <h1>Data buku masih kosong</h1> \
+            </section> \
+        </section>"
+    }
+
+    if ( unreadBookItemHtml == '' ) unreadBookItemHtml = "<section class='book'> \
+        <section> \
+            <h1>-</h1> \
+        </section> \
+    </section>"
+
+    if ( legibleBookItemHtml == '' ) legibleBookItemHtml = "<section class='book'> \
+        <section> \
+            <h1>-</h1> \
+        </section> \
+    </section>"
+
+    unreadBookDom.insertAdjacentHTML("afterbegin", unreadBookItemHtml)
+    legibleBookDom.insertAdjacentHTML("afterbegin", legibleBookItemHtml)
 }
