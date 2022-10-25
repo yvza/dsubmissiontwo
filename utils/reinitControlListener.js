2c477835e331d5c2e219cba6e5ffa204c236572b
yuza <akhmadyuza@gmail.com>
2022-10-25 16:36:07 +0700

    adding edit button listener

--- utils/reinitControlListener.js
+++ utils/reinitControlListener.js
@@ -1,5 +1,6 @@
 import { moveRack } from '../modules/moveRack.js'
 import { deleteBook } from '../modules/deleteBook.js'
+import { editBook } from '../modules/editBook.js'
 
 export function reinitControlListener(){
     // listening to move rack button
@@ -17,6 +18,15 @@ export function reinitControlListener(){
         button.addEventListener('click', () => {
             let idBook = button.value
             deleteBook(idBook)
+        })
+    })
+
+    // listening to edit book button
+    let getEditButton = document.querySelectorAll('.editTitle')
+    getEditButton.forEach(button => {
+        button.addEventListener('click', () => {
+            let idBook = button.value
+            editBook(idBook)
         })
     })
 }
